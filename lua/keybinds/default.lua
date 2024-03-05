5613b7bca03dc43a30cfe801e8e88bfbb318ebc0
David Saah <dasorange.hope@gmail.com>
2024-03-05 15:34:28 +0000

    refactor: create assign and run functions

--- lua/keybinds/default.lua
+++ lua/keybinds/default.lua
@@ -1,22 +1,34 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+local assign = vim.keymap.set
+local run = vim.cmd
+
+assign('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+assign('n', '[c', function()
+  require('treesitter-context').go_to_context()
+end, { silent = true })
+
+assign('n', '<F5>', function()
+  run 'UndotreeToggle'
+  run 'UndotreeFocus'
+end, { silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+assign('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+assign('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+assign('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+assign('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- TIP: Disable arrow keys in normal mode
-vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
-vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+assign('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+assign('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+assign('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+assign('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+assign('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+assign('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+assign('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+assign('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
--- lua/keybinds/telescope.lua
+++ lua/keybinds/telescope.lua
@@ -1,18 +1,20 @@
+local assign = vim.keymap.set
+
 -- See `:help telescope.builtin`
 local builtin = require 'telescope.builtin'
-vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+assign('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+assign('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+assign('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+assign('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+assign('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+assign('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+assign('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+assign('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+assign('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+assign('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
 -- Slightly advanced example of overriding default behavior and theme
-vim.keymap.set('n', '<leader>/', function()
+assign('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
@@ -22,7 +24,7 @@ end, { desc = '[/] Fuzzily search in current buffer' })
 
 -- Also possible to pass additional configuration options.
 --  See `:help telescope.builtin.live_grep()` for information about particular keys
-vim.keymap.set('n', '<leader>s/', function()
+assign('n', '<leader>s/', function()
   builtin.live_grep {
     grep_open_files = true,
     prompt_title = 'Live Grep in Open Files',
@@ -30,6 +32,6 @@ vim.keymap.set('n', '<leader>s/', function()
 end, { desc = '[S]earch [/] in Open Files' })
 
 -- Shortcut for searching your neovim configuration files
-vim.keymap.set('n', '<leader>sn', function()
+assign('n', '<leader>sn', function()
   builtin.find_files { cwd = vim.fn.stdpath 'config' }
 end, { desc = '[S]earch [N]eovim files' })
