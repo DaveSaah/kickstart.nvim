bd75261168f13b102236701148de1112a5d41869
David Saah <dasorange.hope@gmail.com>
2024-03-05 13:58:06 +0000

    feat: modularise kickstart.nvim

---
+++ lua/kickstart/plugins/init.lua
@@ -0,0 +1,18 @@
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  require 'kickstart.plugins.todo-comments',
+  require 'kickstart.plugins.mini',
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.treesitter',
+  require 'kickstart.plugins.autocomplete',
+  require 'kickstart.plugins.autoformat',
+  require 'kickstart.plugins.gitsigns',
+  require 'kickstart.plugins.which-key',
+  require 'kickstart.plugins.telescope',
+}
