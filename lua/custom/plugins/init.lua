f788c569a6731ce38d880e2e04bb2a6cb59c3fb4
David Saah <davesaah@gmail.com>
2024-12-09 14:00:43 +0000

    plugin: add vimtex

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -27,4 +27,14 @@ return {
     'nvim-treesitter/nvim-treesitter-context',
     event = 'VeryLazy',
   },
+
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
 }
