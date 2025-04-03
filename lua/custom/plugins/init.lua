dc124c24d283ad3fb59da266bb544efd5d181234
David Saah <davesaah@gmail.com>
2025-04-03 22:41:12 +0000

    rm wakatime

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,10 +1,4 @@
 return {
-  -- wakatime
-  {
-    'wakatime/vim-wakatime',
-    lazy = false,
-  },
-
   require 'custom.plugins.catppuccin',
   require 'custom.plugins.lualine',
   require 'custom.plugins.autopairs',
